36ebe23a2254b46f5a698e22718038fb8b50fd7d
uye <99072975+ABA2396@users.noreply.github.com>
2024-12-14 20:54:49 +0800

    添加项目文件。

---
+++ BookSelectionOptimizer/MainForm.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace BookSelectionOptimizer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.txtTargetAmount = new System.Windows.Forms.TextBox();
+            this.txtMinQty = new System.Windows.Forms.TextBox();
+            this.txtMaxQty = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.dgvResults = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.书名 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.数量 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.小计 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.allowZeroCheck = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(133, 27);
+            this.txtFilePath.Multiline = true;
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(452, 37);
+            this.txtFilePath.TabIndex = 0;
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(606, 27);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(119, 37);
+            this.btnSelectFile.TabIndex = 1;
+            this.btnSelectFile.Text = "选择文件";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // txtTargetAmount
+            // 
+            this.txtTargetAmount.Location = new System.Drawing.Point(133, 85);
+            this.txtTargetAmount.Multiline = true;
+            this.txtTargetAmount.Name = "txtTargetAmount";
+            this.txtTargetAmount.Size = new System.Drawing.Size(100, 37);
+            this.txtTargetAmount.TabIndex = 2;
+            // 
+            // txtMinQty
+            // 
+            this.txtMinQty.Location = new System.Drawing.Point(316, 85);
+            this.txtMinQty.Multiline = true;
+            this.txtMinQty.Name = "txtMinQty";
+            this.txtMinQty.Size = new System.Drawing.Size(100, 37);
+            this.txtMinQty.TabIndex = 3;
+            // 
+            // txtMaxQty
+            // 
+            this.txtMaxQty.Location = new System.Drawing.Point(485, 85);
+            this.txtMaxQty.Multiline = true;
+            this.txtMaxQty.Name = "txtMaxQty";
+            this.txtMaxQty.Size = new System.Drawing.Size(100, 37);
+            this.txtMaxQty.TabIndex = 4;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(606, 87);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(119, 37);
+            this.btnRun.TabIndex = 5;
+            this.btnRun.Text = "开始凑单";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // dgvResults
+            // 
+            this.dgvResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResults.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.书名,
+            this.数量,
+            this.小计});
+            this.dgvResults.Location = new System.Drawing.Point(40, 149);
+            this.dgvResults.Name = "dgvResults";
+            this.dgvResults.RowHeadersWidth = 82;
+            this.dgvResults.RowTemplate.Height = 33;
+            this.dgvResults.Size = new System.Drawing.Size(685, 329);
+            this.dgvResults.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "文件路径";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "目标金额（分）";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(257, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "数量下限";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(426, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "数量上限";
+            // 
+            // 书名
+            // 
+            this.书名.HeaderText = "书名";
+            this.书名.MinimumWidth = 10;
+            this.书名.Name = "书名";
+            this.书名.Width = 350;
+            // 
+            // 数量
+            // 
+            this.数量.HeaderText = "数量";
+            this.数量.MinimumWidth = 10;
+            this.数量.Name = "数量";
+            // 
+            // 小计
+            // 
+            this.小计.HeaderText = "小计（元）";
+            this.小计.MinimumWidth = 10;
+            this.小计.Name = "小计";
+            // 
+            // allowZeroCheck
+            // 
+            this.allowZeroCheck.AutoSize = true;
+            this.allowZeroCheck.Location = new System.Drawing.Point(395, 128);
+            this.allowZeroCheck.Name = "allowZeroCheck";
+            this.allowZeroCheck.Size = new System.Drawing.Size(72, 16);
+            this.allowZeroCheck.TabIndex = 8;
+            this.allowZeroCheck.Text = "允许为 0";
+            this.allowZeroCheck.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(765, 486);
+            this.Controls.Add(this.allowZeroCheck);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvResults);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txtMaxQty);
+            this.Controls.Add(this.txtMinQty);
+            this.Controls.Add(this.txtTargetAmount);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.txtFilePath);
+            this.Name = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox txtTargetAmount;
+        private System.Windows.Forms.TextBox txtMinQty;
+        private System.Windows.Forms.TextBox txtMaxQty;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.DataGridView dgvResults;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 书名;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 数量;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 小计;
+        private System.Windows.Forms.CheckBox allowZeroCheck;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BookSelectionOptimizer/MainForm.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+using Microsoft.Z3;
+using Context = Microsoft.Z3.Context;
+using Status = Microsoft.Z3.Status;
+
+namespace BookSelectionOptimizer
+{
+    public partial class MainForm : Form
+    {
+        private DateTime _startTime;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = @"Excel Files|*.xlsx;*.xls";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtFilePath.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private async void btnRun_Click(object sender, EventArgs e)
+        {
+            var filePath = txtFilePath.Text;
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show(@"请选择有效的 Excel 文件！", @"错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var booksData = LoadBooksFromExcel(filePath);
+            if (booksData == null || booksData.Count == 0)
+            {
+                MessageBox.Show(@"未找到有效的书籍数据！", @"错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // 提取书名和价格列表
+            var books = booksData.Select(b => b.book).ToList();
+            var prices = booksData.Select(b => b.price).ToList();
+
+            if (!int.TryParse(txtTargetAmount.Text, out var targetAmount) ||
+                !int.TryParse(txtMinQty.Text, out var minQty) ||
+                !int.TryParse(txtMaxQty.Text, out var maxQty))
+            {
+                MessageBox.Show(@"请填写有效的数字！", @"错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var allowZero = allowZeroCheck.Checked;
+
+            btnRun.Enabled = false;
+            _startTime = DateTime.Now;
+            timer1.Start();
+            var results = await Task.Run(() => SolveBookSelectionWithZ3(books, prices, targetAmount, minQty, maxQty, allowZero));
+            btnRun.Enabled = true;
+            timer1.Stop();
+            btnRun.Text = @"开始凑单";
+            MessageBox.Show($@"计算完成！耗时 {DateTime.Now - _startTime:hh\:mm\:ss}");
+
+            if (results == null || results.Count == 0)
+            {
+                MessageBox.Show(@"未找到符合条件的组合！", @"结果", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                dgvResults.Rows.Clear();
+                foreach (var (book, qty, cost) in results)
+                {
+                    dgvResults.Rows.Add(book, qty, cost / (decimal)100.0);
+                }
+
+                var totalSum = results.Sum(item => item.cost);
+                SaveResultsToExcel(filePath, results, totalSum);
+                if (totalSum < targetAmount)
+                {
+                    MessageBox.Show($@"书单价格仍小于指定价格，可能为目标无解，或给定的数量上限不足，当前金额：{totalSum / 100.0:0.##}，目标金额：{targetAmount / 100.0:0.##}", @"警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    MessageBox.Show($@"计算完成！目标金额：{totalSum / 100.0:0.##}", @"成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private static List<(string book, int qty, int cost)> SolveBookSelectionWithZ3(List<string> books, List<int> prices, int totalPrice, int n, int m, bool allowZero)
+        {
+            // 使用 Z3 定义优化问题
+            using (var ctx = new Context())
+            {
+                var x = new IntExpr[prices.Count];
+                for (var i = 0; i < prices.Count; i++)
+                {
+                    x[i] = ctx.MkIntConst($"x_{i}");
+                }
+
+                var opt = ctx.MkOptimize();
+
+                // 添加约束 n <= x[i] <= m 或 x[i] == 0
+                for (var i = 0; i < prices.Count; i++)
+                {
+                    if (allowZero)
+                    {
+                        opt.Add(ctx.MkOr(
+                            ctx.MkAnd(ctx.MkGe(x[i], ctx.MkInt(n)), ctx.MkLe(x[i], ctx.MkInt(m))),
+                            ctx.MkEq(x[i], ctx.MkInt(0))
+                        ));
+                    }
+                    else
+                    {
+                        opt.Add(ctx.MkAnd(
+                            ctx.MkGe(x[i], ctx.MkInt(n)),
+                            ctx.MkLe(x[i], ctx.MkInt(m))
+                        ));
+                    }
+                    
+                }
+
+                // 添加总价格约束
+                ArithExpr totalCost = ctx.MkInt(0);
+                for (var i = 0; i < prices.Count; i++)
+                {
+                    totalCost = ctx.MkAdd(totalCost, ctx.MkMul(x[i], ctx.MkInt(prices[i])));
+                }
+                opt.Add(ctx.MkLe(totalCost, ctx.MkInt(totalPrice)));
+
+                // 设置目标函数
+                opt.MkMaximize(totalCost);
+
+                // 求解
+                if (opt.Check() != Status.SATISFIABLE) return null;
+                {
+                    var model = opt.Model;
+                    var selectedBooks = new List<(string book, int qty, int cost)>();
+
+                    for (var i = 0; i < prices.Count; i++)
+                    {
+                        var qty = int.Parse(model.Evaluate(x[i]).ToString());
+                        if (qty < 0) continue;
+                        var cost = qty * prices[i];
+                        selectedBooks.Add((books[i], qty, cost));
+                    }
+                    return selectedBooks;
+                }
+
+            }
+        }
+
+        private static List<(string book, int price)> LoadBooksFromExcel(string filePath)
+        {
+            var books = new List<(string book, int price)>();
+
+            try
+            {
+                using (var workbook = new XLWorkbook(filePath))
+                {
+                    var sheet = workbook.Worksheet(1);
+                    var headers = sheet.FirstRowUsed()?.Cells().Select(cell => cell.GetValue<string>()).ToList();
+
+                    // 根据列名获取索引
+                    if (headers != null)
+                    {
+                        var bookNameIndex = headers.IndexOf("书名") + 1; // ClosedXML 索引从 1 开始
+                        var priceIndex = headers.IndexOf("定价") + 1;
+
+                        if (bookNameIndex == 0 || priceIndex == 0)
+                        {
+                            MessageBox.Show(@"Excel 文件中未找到列名 '书名' 或 '定价'！", @"错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return null;
+                        }
+
+                        // 读取数据
+                        books.AddRange(from row in sheet.RowsUsed().Skip(1) let bookName = row.Cell(bookNameIndex).GetValue<string>() let price = (int)(row.Cell(priceIndex).GetValue<decimal>() * 100) select (bookName, price));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($@"无法加载 Excel 文件: {ex.Message}", @"错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+
+            return books;
+        }
+
+        private static void SaveResultsToExcel(string originalFilePath, List<(string book, int qty, int cost)> results, int totalCost)
+        {
+            var directory = Path.GetDirectoryName(originalFilePath);
+            if (directory == null) return;
+            var outputFilePath = Path.Combine(directory, "凑单结果.xlsx");
+
+            try
+            {
+                using (var workbook = new XLWorkbook())
+                {
+                    var sheet = workbook.Worksheets.Add("结果");
+                    sheet.Cell(1, 1).Value = "书名";
+                    sheet.Cell(1, 2).Value = "数量";
+                    sheet.Cell(1, 3).Value = "小计（元）";
+
+                    var row = 2;
+                    foreach (var result in results)
+                    {
+                        sheet.Cell(row, 1).Value = result.book;
+                        sheet.Cell(row, 2).Value = result.qty;
+                        sheet.Cell(row, 3).Value = result.cost / 100.0; // 转回元
+                        row++;
+                    }
+
+                    sheet.Cell(row, 2).Value = "总计（元）";
+                    sheet.Cell(row, 3).Value = totalCost / 100.0;
+
+                    workbook.SaveAs(outputFilePath);
+                }
+
+                MessageBox.Show($@"结果已保存到 {outputFilePath}", @"成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($@"无法保存结果文件: {ex.Message}", @"错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var elapsed = DateTime.Now - _startTime;
+            btnRun.Text = $@"计算中({elapsed:hh\:mm\:ss})";
+        }
+    }
+}
